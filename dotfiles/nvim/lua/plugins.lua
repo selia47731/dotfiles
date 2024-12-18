c9c4d97efcb352ddbce57c8b2d1a5f4ba218f587
selia47731 <aquselia47331@gmail.com>
2024-12-18 23:36:29 +0900

    nvim/lua/plugins.luaを削除し、treesitter.luaへ記述

--- dotfiles/nvim/lua/plugins.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { import = "plugins.vimtex" },
-  "nvim-treesitter/nvim-treesitter",
-}
---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      ensure_installed = { "agda", "c", "lua", "python", "vim", "markdown", "query", "latex", "php", "perl", "rust" },
+      highlight = {
+        enable = true,
+        disable = {},
+      },
+      indent = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
