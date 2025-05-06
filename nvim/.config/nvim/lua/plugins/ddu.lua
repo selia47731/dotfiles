ba25325ee56eabc3e10b92d69033217a8ad60f7a
selia47731 <aquselia47331@gmail.com>
2025-05-06 23:45:29 +0900

    ddu.lua: 利用できるように改修工事

--- nvim/.config/nvim/lua/plugins/ddu.lua
+++ nvim/.config/nvim/lua/plugins/ddu.lua
@@ -4,26 +4,63 @@ return {
   dependencies = {
     "vim-denops/denops.vim",
     "Shougo/ddu-ui-ff",
-    "Shougo/ddu-source-file_rec",
-    "Shougo/ddu-filter-matcher_substring",
     "Shougo/ddu-kind-file",
+    "Shougo/ddu-source-file_rec",
+    "Shougo/ddu-source-rg",
+    "Shougo/ddu-filter-matcher_substring",
+    "uga-rosa/ddu-filter-converter_devicon",
+    "lambdalisue/kensaku.vim",
+    "Milly/ddu-filter-kensaku",
   },
   config = function()
     vim.cmd[[
       call ddu#custom#patch_global(#{
         \   ui: 'ff',
-        \   sources: [#{name: 'file_rec', params: {}}],
+        \   _: #{
+        \     matters: [ 'matcher_kensaku' ],
+        \   },
+        \   uiParams: #{
+        \     ff: #{
+        \       split: 'floating',
+        \     },
+        \   },
+        \   sources: [#{ name: 'file_rec' }],
         \   sourceOptions: #{
         \     _: #{
-        \       matchers: ['matcher_substring'],
+        \       matchers: [ 'matcher_substring' ],
+        \     },
+        \     file_rec: #{
+        \       matchers: [ 'matcher_substring', 'macher_kensaku' ],
+        \       converters: [ 'converter_devicon' ],
+        \       path: expand("~"),
+        \     },
+        \   },
+        \   sourceParams: #{
+        \     rg: #{
+        \       args: ['--column', '--no-heading', '--color', 'never'],
         \     },
         \   },
         \   kindOptions: #{
         \     file: #{
         \       defaultAction: 'open',
         \     },
-        \   }
+        \   },
+        \   filtersParams: #{
+        \     macher_kensaku: #{
+        \       hightlightMatched: 'Search',
+        \     },
+        \   },
         \ })
     ]]
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ddu-ff",
+      callback = function()
+        local opts = { noremap =true, silent =true, buffer = true }
+        vim.keymap.set("n", "<CR>", [[<cmd>call ddu#ui#ff#do_action('itemAction')<CR>]], opts)
+        vim.keymap.set("n", "<Space>", [[<cmd>call ddu#ui#ff#do_action('toggleSelectItem')<CR>]], opts)
+        vim.keymap.set("n", "i", [[<cmd>call ddu#ui#ff#do_action('openFilterWindow')<CR>]], opts)
+        vim.keymap.set("n", "q", [[<cmd>call ddu#ui#ff#do_action('quit')<CR>]], opts)
+      end,
+    })
   end,
 }
