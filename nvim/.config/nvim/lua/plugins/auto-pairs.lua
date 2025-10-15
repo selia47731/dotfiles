68580820600369b4744dd488330c75af34fe4b55
ie42-yk <e215742@ie.u-ryukyu.ac.jp>
2025-10-15 17:56:58 +0900

    auto-pairs.lua: 導入

---
+++ nvim/.config/nvim/lua/plugins/auto-pairs.lua
@@ -0,0 +1,24 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    require("nvim-autopairs").setup({
+      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      disable_in_macro = false,
+      disable_in_visualblock = false,
+      disable_in_replace_mode = true,
+      ignored_next_char =[=[[%w%%%'%[%"%.%`%$]]=],
+      enable_moveright = true,
+      enable_afterquote = true,
+      enable_check_bracket_line = true,
+      enable_bracket_in_quote = true,
+      enable_abbr = false,
+      break_undo = true,
+      check_ts = false,
+      map_cr = false,
+      map_bs = false,
+      map_c_h = false,
+      map_c_w = false,
+    })
+  end,
+}
