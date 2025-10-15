ba70d1d2cd2eb327b48715530ee355a47d113122
selia47731 <aquselia47331@gmail.com>
2025-10-15 17:59:04 +0900

    treesitter.lua: Neovim 0.11対応

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,29 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      ensure_installed = { "agda", "c", "lua", "python", "vim", "markdown_inline", "query", "latex", "php", "perl", "rust", "csv" },
-      highlight = {
-        enable = true,
-        disable = {},
-      },
-      indent = {
-        enable = true,
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  init = function()
+    require("nvim-treesitter").setup({
+        install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "treesitter"),
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(event)
+        local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+        if not ok then return end
+        local filetype = vim.bo[event.buf].filetype
+        local lang = vim.treesitter.language.get_lang(filetype)
+        nvim_treesitter.install({lang}):await(function(err)
+          if err then
+            vim.notify('Treesitter install error for filetype' .. filetype .. ' err: ' .. err)
+            return
+          end
+          pcall(vim.treesitter.start, event.buf)
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end)
+      end,
+    })
+  end,
 }
