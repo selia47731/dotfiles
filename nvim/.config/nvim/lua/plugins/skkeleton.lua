74988fcbb4e5d9c1511e915e4d1a8bb8d49b37d5
selia47731 <aquselia47331@gmail.com>
2025-10-21 20:56:38 +0900

    plugins/skkeleton.lua: 利用するための設定をした

--- nvim/.config/nvim/lua/plugins/skkeleton.lua
+++ nvim/.config/nvim/lua/plugins/skkeleton.lua
@@ -6,16 +6,18 @@ return {
     { "delphinus/skkeleton_indicator.nvim", opts = {} }
   },
   config = function()
-    vim.cmd([[
-      call skkeleton#config({
-        \ 'sources': ['skk_dictionary'],
-        \ 'globalKanaTableFiles': ['~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings/kana-rule.conf'],
-        \ 'lowercaseMap': {
-        \    '+': ';',
-        \    ':': ';',
-        \ },
-        \ 'userDictionary': '~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/skk-jisyo.utf8',
-        \ })
-    ]])
+    local json_text = table.concat(vim.fn.readfile(vim.fn.expand("~/.skkeleton/azik_rule/azik_kana.json")), "\n")
+    local azik_table = vim.fn.json_decode(json_text)
+    vim.fn['skkeleton#register_kanatable']("azik", azik_table, true)
+    vim.fn['skkeleton#config']{
+      globalDictionaries = {
+        "~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/SKK-JISYO.L",
+      },
+      kanaTable = "azik",
+      lowercaseMap = {
+        ['+'] = ';',
+      },
+    }
+    vim.keymap.set('i', '<C-j>', '<Plug>(skkeleton-toggle)', { noremap = false })
   end,
 }
