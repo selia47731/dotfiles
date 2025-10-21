e41ae72f633c33e65cf32e2731815947595bea55
ie42-yk <e215742@ie.u-ryukyu.ac.jp>
2025-10-21 20:58:52 +0900

    WezTermへ移行

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return{
     require('lualine').setup({
       options = {
         icons_enabled = true,
-        theme = 'sakura',
+        theme = 'auto',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {},
