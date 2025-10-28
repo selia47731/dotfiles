516a819546f841b30935f012e53a503fe73922a8
selia47731 <aquselia47331@gmail.com>
2025-10-28 10:21:08 +0900

    plugins/neocord.lua: update

--- nvim/.config/nvim/lua/plugins/neocord.lua
+++ nvim/.config/nvim/lua/plugins/neocord.lua
@@ -4,25 +4,41 @@ return {
   config = function()
     require("neocord").setup({
       -- General options
-      logo                = "auto",                     -- "auto" or url
-      logo_tooltip        = nil,                        -- nil or string
-      main_image          = "language",                 -- "language" or "logo"
-      client_id           = "1157438221865717891",      -- Use your own Discord application client id (not recommended)
-      log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-      debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-      blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-      file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-      show_time           = true,                       -- Show the timer
-      global_timer        = false,                      -- if set true, timer won't update when any event are triggered
+      logo                = "auto",
+      logo_tooltip        = nil,
+      main_image          = "language",
+      log_level           = nil,
+      debounce_timeout    = 10,
+      blacklist           = {},
+      file_assets         = {},
+      show_time           = true,
+      global_timer        = false,
 
       -- Rich Presence text options
-      editing_text        = "Editing files",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-      file_explorer_text  = "Browsing files",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-      git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-      plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-      reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-      workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-      line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+      editing_text        = function()
+        local ft = vim.bo.filetype or "unknown"
+        return "Editing a " .. ft .. " file"
+      end,
+      file_explorer_text  = function()
+        local ft = vim.bo.filetype or "unknown"
+        return "Browsing " .. ft .. " files"
+      end,
+      git_commit_text     = function()
+        local branch = vim.b.gitsigns_head or "unknown"
+        return "Committing to " .. branch
+      end,
+      plugin_manager_text = function()
+        return "Managing plugins"
+      end,
+      reading_text        = function()
+        local ft = vim.bo.filetype or "unknown"
+        return "Reading a " .. ft .. " file"
+      end,
+      workspace_text      = function()
+        local ft = vim.bo.filetype or "unknown"
+        return "Working on " .. ft .. " project"
+      end,
+      line_number_text    = "Line %s out of %s",
   })
   end,
 }
