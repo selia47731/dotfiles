003492caecfd8e36e458021a85044a6ab6b59b1e
ie42-yk <e215742@ie.u-ryukyu.ac.jp>
2025-10-28 16:39:02 +0900

    plugins/skkeleton.lua: azik対応

--- nvim/.config/nvim/lua/plugins/skkeleton.lua
+++ nvim/.config/nvim/lua/plugins/skkeleton.lua
@@ -8,15 +8,30 @@ return {
       local json_text = table.concat(vim.fn.readfile(vim.fn.expand("~/.skkeleton/azik_rule/azik_kana.json")), "\n")
       local azik_table = vim.fn.json_decode(json_text)
       vim.fn['skkeleton#register_kanatable']("azik", azik_table, true)
+      vim.g['skkeleton#mapped_keys'] = vim.g['skkeleton#mapped_keys'] or {}
+      table.insert(vim.g['skkeleton#mapped_keys'], "\\")
+      table.insert(vim.g['skkeleton#mapped_keys'], "/")
+      table.insert(vim.g['skkeleton#mapped_keys'], "<Space>")
+      table.insert(vim.g['skkeleton#mapped_keys'], "<Esc>")
+      vim.fn['skkeleton#register_keymap']("input", "\\", 'katakana')
+      vim.fn['skkeleton#register_keymap']("input", "/", 'abbrev')
+      vim.fn['skkeleton#register_keymap']("input", ";", 'henkanPoint')
+      vim.fn['skkeleton#register_keymap']("henkan", "<Space>", 'henkanForward')
+      vim.fn['skkeleton#register_keymap']("henkan", "<Esc>", 'cancel')
+
       vim.fn['skkeleton#config']{
         globalDictionaries = {
           "~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/SKK-JISYO.L",
         },
+        userDictionary = "~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/skk-jisyo.utf8",
         kanaTable = "azik",
         lowercaseMap = {
           ['+'] = ';',
         },
+        immediatelyCancel = true,
+        showCandidatesCount = 1,
       }
+
       vim.keymap.set('i', '<C-j>', '<Plug>(skkeleton-toggle)', {noremap = true})
       vim.keymap.set('c', '<C-j>', '<Plug>(skkeleton-toggle)', {noremap = true})
     end,
