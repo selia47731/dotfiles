21d1e10b902fabec780165ab11b51f3cd4797819
selia47731 <aquselia47331@gmail.com>
2025-11-07 12:50:18 +0900

    ddc.lua ddu.lua: vim.cmdからvim.fmへ書き直した

--- nvim/.config/nvim/lua/plugins/ddc.lua
+++ nvim/.config/nvim/lua/plugins/ddc.lua
@@ -15,47 +15,44 @@ return {
     "vim-skk/skkeleton",
   },
   config = function()
-    vim.cmd([[
-      call ddc#custom#patch_global('ui', 'native')
-      call ddc#custom#patch_global('sources', ['lsp', 'around', 'buffer', 'skkeleton'])
-      call ddc#custom#patch_global('sourceOptions', #{
-        \ _: #{
-        \   matchers: ['matcher_head'],
-        \   sorters: ['sorter_rank'],
-        \   converters: ['converter_remove_overlap'],
-        \ },
-        \ lsp: #{
-        \   mark: 'lsp',
-        \   forceCompletionPattern: '\.\w*|:\w*|->\w*',
-        \ },
-        \ around: #{ mark: 'around' },
-        \ buffer: #{ mark: 'buffer' },
-        \ skkeleton: #{
-        \   mark: 'skkeleton',
-        \   matchers: [],
-        \   sorters: [],
-        \   converters: [],
-        \   isVolatile: v:true,
-        \   minAutoCompleteLength: 1,
-        \ },
-        \ })
-      call ddc#custom#patch_global('sourceParams', #{
-        \ lsp: #{
-        \   snippetEngine: denops#callback#register({
-        \     body -> vsnip#anonymous(body)
-        \   }),
-        \   enableResolveItem: v:true,
-        \   enableAdditionalTextEdits: v:true,
-        \ },
-        \ around: #{ maxSize: 500 },
-        \ buffer: #{
-        \ requireSameFiletype: v:true,
-        \ limitAltBuf: v:true,
-        \ forceCollect: v:true,
-        \ },
-        \ })
-
-      call ddc#enable()
-    ]])
+    vim.fn["ddc#custom#patch_global"]('ui', 'native')
+    vim.fn["ddc#custom#patch_global"]('sources', { 'lsp', 'around', 'buffer', 'skkeleton' })
+    vim.fn["ddc#custom#patch_global"]('sourceOptions', {
+      _ = {
+        matchers = { 'matcher_head' },
+        sorters = { 'sorter_rank' },
+        converters = { 'converter_remove_overlap' },
+      },
+      lsp = {
+        mark = 'lsp',
+        forceCompletionPattern = [[\.\w*|:\w*|->\w*]],
+      },
+      around = { mark = 'around' },
+      buffer = { mark = 'buffer' },
+      skkeleton = {
+        mark = 'skkeleton',
+        matchers = {},
+        sorters = {},
+        converters = {},
+        isVolatile = true,
+        minAutoCompleteLength = 1,
+      },
+    })
+    vim.fn["ddc#custom#patch_global"]('sourceParams', {
+      lsp = {
+        snippetEngine = function(body)
+          return vim.fn["vsnip#anonymous"](body)
+        end,
+        enableResolveItem = true,
+        enableAdditionalTextEdits = true,
+      },
+      around = { maxSize = 500 },
+      buffer = {
+        requireSameFiletype = true,
+        limitAltBuf = true,
+        forceCollect = true,
+      },
+    })
+    vim.fn["ddc#enable"]()
   end,
 }
