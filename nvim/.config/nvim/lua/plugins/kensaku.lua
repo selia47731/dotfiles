b3ae0f95355f6a88a63ce0bdde8282814e6c05b9
selia47731 <aquselia47331@gmail.com>
2026-06-01 18:27:02 +0900

    plugins/kensaku.lua: 設定の調整

--- nvim/.config/nvim/lua/plugins/kensaku.lua
+++ nvim/.config/nvim/lua/plugins/kensaku.lua
@@ -1,14 +1,16 @@
 return {
-  "lambdalisue/kensaku.vim",
+  "lambdalisue/vim-kensaku",
   dependencies = {
     "vim-denops/denops.vim",
+    "lambdalisue/vim-kensaku-command"
   },
-  config =function()
-    vim.cmd[[
-      function! Search(value) abort
-        let @/ = a:value
-        nomal! n
-      endfunction
-    ]]
-  end,
+  config = function()
+    vim.api.nvim_create_user_command("Search", function(opts)
+      local pattern = vim.fn["kensaku#query"](opts.args)
+      vim.fn.setreg("/", pattern)
+      vim.cmd("normal! n")
+    end, {
+      nargs = "+"
+    })
+  end
 }
