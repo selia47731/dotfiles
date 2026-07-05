eb46ab1067f077489c90a2c7dbc5482adbb689ef
selia47731 <aquselia47331@gmail.com>
2026-07-05 17:07:13 +0900

    agda使わなくなったのでけしけし

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -59,15 +59,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
--- nvim agda settings
-local agda_group = vim.api.nvim_create_augroup("filetypedetect_agda", { clear = true })
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = agda_group,
-  pattern = "*.agda",
-  callback = function()
-    vim.bo.filetype = "agda"
-  end,
-})
 
 -- nvim jupytext settings
 local ipynb_group = vim.api.nvim_create_augroup("filetypedetect_ipynb", { clear = true })
