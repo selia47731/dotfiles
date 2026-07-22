937b33a7914b34b29b0b9163531e9ae3ccca9339
selia47731 <aquselia47331@gmail.com>
2026-07-22 10:44:21 +0900

    lua5.1回りの調整

--- home-manager/selia/module/dev.nix
+++ home-manager/selia/module/dev.nix
@@ -1,17 +1,33 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    cmake
-    lua5_1
-    luarocks
-    deno
-    nodejs
-    tree-sitter
-    stylua
-    selene
-    shellcheck
-    automake
-    boost
-  ];
+  home = {
+    packages = with pkgs; [
+      cmake
+      lua5_1
+      lua51Packages.luarocks
+      deno
+      nodejs
+      tree-sitter
+      stylua
+      selene
+      shellcheck
+      automake
+      boost
+    ];
+
+    file = {
+      ".luarocks/config-5.1.lua".text = ''
+        lua_version = "5.1"
+
+        arch = "macosx-aarch64"
+        processor = "aarch64"
+        target_cpu = "aarch64"
+      '';
+
+      ".luarocks/default-lua-version.lua".text = ''
+        return "5.1"
+      '';
+    };
+  };
 }
