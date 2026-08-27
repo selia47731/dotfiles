d57732401e22943a7c3b8acffbd7a5b3ca537197
selia47731 <aquselia47331@gmail.com>
2026-08-27 17:02:31 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -1,37 +1,91 @@
 {
-  description = "selia's NixOS configuration";
+  description = "selia's dotfiles flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      ...
-    }:
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    nix-darwin,
+    home-manager,
+    nix-homebrew,
+    ...
+  }:
+    let
+      hosts = {
+        mac = {
+          hostPlatform = "aarch64-darwin";
+          user = "selia";
+          hostname = "selia";
+          homeDirectory = "/Users/selia";
+        };
+
+        nixos-arm64 = {
+          system = "aarch64-linux";
+          user = "selia";
+          hostname = "nixos";
+          homeDirectory = "/home/selia";
+        };
+      };
+    in {
+      darwinConfigurations${hosts.mac.hostname} = {
+        nix-darwin.lib.darwinSystem {
+          specialArgs = {
+          inherit self;
+          inherit (hosts.mac) user hostPlatform homeDirectory;
+          };
+
+          modules = [
+            ./nix-darwin
+
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+
+          ];
+        };
+      };
+
+      nixosConfigurations.${hosts.nixos-arm64.hostname} =
+      nixpkgs.lib.nlib.nixosSystem {
+        system = hosts.nixos-arm64.system;
+
+        specialArgs = {
+          inherit self;
+          inherit (hosts.nixos-arm64) user system homeDirectory;
+          hostPlatform = hosts.nixos-arm64.system;
+        };
 
         modules = [
-          ./configuration.nix
+          ./nixos
 
           home-manager.nixosModules.home-manager
-
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
 
-              users.selia = import ./home.nix;
+              extraSpecialArgs = {
+                inherit self inputs;
+                inherit (hosts.nixos-arm64) user hostname;
+                hostPlatform = hosts.nixos-arm64.system;
+              };
+
+              users.${hosts.nixos-arm64.user} =
+                import ./home-manager/home.nix;
             };
           }
         ];
--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -2,6 +2,7 @@
   self,
   user,
   hostPlatform,
+  homeDirectory,
   ...
 }:
 {
